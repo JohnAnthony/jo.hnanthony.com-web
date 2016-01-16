57fb28f7df8c7e25a08c9bb07a34e099253f2db9
John Anthony <john@jo.hnanthony.com>
2016-01-16 12:37:49 +0000

    Added 404 and 500 errors

--- root.js
+++ root.js
@@ -4,12 +4,25 @@ var vhost = require('vhost');
 
 var ja = express();
 ja.use(express.static(path.join(__dirname, "jo.hnanthony.com")));
+ja.use(function(req, res) {
+	res.send('404: Page not Found', 404);
+});
+ja.use(function(error, req, res, next) {
+	res.send('500: Internal Server Error', 500);
+});
 
 var ks = express();
 ks.use(express.static(path.join(__dirname, "kieransanderson.xyz")));
+ks.use(function(req, res) {
+	res.send('404: Page not Found', 404);
+});
+ks.use(function(error, req, res, next) {
+	res.send('500: Internal Server Error', 500);
+});
 
 var app = express();
 app.use(vhost("jo.hnanthony.com", ja));
+app.use(vhost("www.jo.hnanthony.com", ja));
 app.use(vhost("kieransanderson.xyz", ks));
 app.use(vhost("www.kieransanderson.xyz", ks));
 app.listen(8080);
