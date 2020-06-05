66b0c556e3c67011e83d89a0588bc5ba3b15085b
John Anthony <john@jo.hnanthony.com>
2020-06-06 00:23:03 +0100

    switch to bundled response

--- main.go
+++ main.go
@@ -3,28 +3,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"html/template"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	index, err := Asset("static/index.html")
+func buildBody() ([]byte, error) {
+	bg, err := Asset("static/bg.png")
 	if err != nil {
-		log.Fatal(err)
+		return []byte{}, err
 	}
 
-	bg, err := Asset("static/bg.png")
+	templateVars := struct {
+		BG string
+	}{
+		BG: base64.StdEncoding.EncodeToString(bg),
+	}
+
+	var ret []byte
+	buf := bytes.NewBuffer(ret)
+	index, err := Asset("static/index.html.template")
+	if err != nil {
+		return []byte{}, err
+	}
+	tmpl, err := template.New("index").Parse(string(index))
+	if err != nil {
+		return []byte{}, err
+	}
+	err = tmpl.Execute(buf, templateVars)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func main() {
+	body, err := buildBody()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.Writer.Write(index)
-	})
-	r.GET("/bg.png", func(c *gin.Context) {
-		c.Writer.Write(bg)
+		c.Writer.Write(body)
 	})
 	r.Run()
 }
